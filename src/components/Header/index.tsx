aef4a69e2aaa67f7c10447b978162d086be94f92
elias alexandre <elliasallex@gmail.com>
2022-07-12 18:50:04 -0300

    migrts

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -12,65 +12,83 @@ interface HeaderProps {
 
 export function Header({ disableSearchRedirect = false }: HeaderProps) {
   return (
-    <header id={styles.headerContainer}>
-      <nav id="bigJeagle">
-        <div className={styles.logoContainer}>
-          <NextLink href="/dashboard" passHref>
-            <a href="/dashboard">Genesis</a>
-          </NextLink>
-        </div>
-
-        <div>
-          {disableSearchRedirect ? (
-            <form className={styles.searchForm}>
-              <input
-                type="search"
-                id="search"
-                data-target="search"
-                placeholder="Search artist for book"
-                autoFocus
-              />
-              <div className={styles.look}>
-                <FiSearch size={24} color="var(--gray-300)" />
-              </div>
-            </form>
-          ) : (
-            <NextLink href="/search" passHref>
-              <a>
-                <form className={styles.searchForm}>
-                  <input
-                    type="search"
-                    id="search"
-                    data-target="search"
-                    placeholder="Search artist for book"
-                    autoFocus
-                  />
-                  <div className={styles.look}>
-                    <FiSearch size={24} color="var(--gray-300)" />
-                  </div>
-                </form>
-              </a>
+    <div>
+      <header id={styles.headerContainer}>
+        <nav id="bigJeagle">
+          <div className={styles.logoContainer}>
+            <NextLink href="/dashboard" passHref>
+              <a href="/dashboard">Genesis</a>
             </NextLink>
-          )}
-        </div>
+          </div>
 
-        <ul>
-          <li>
-            <Products />
-          </li>
-          <li>
-            <button
-              className={`${styles.button} ${styles.signup}`}
-              type="button"
-            >
-              <span>Sign up</span>
-            </button>
-          </li>
-          <li>
-            <MoreOptions />
-          </li>
-        </ul>
-      </nav>
-    </header>
+          <div>
+            {disableSearchRedirect ? (
+              <form className={styles.searchForm}>
+                <input
+                  type="search"
+                  id="search"
+                  data-target="search"
+                  placeholder="Search artist for book"
+                  autoFocus
+                />
+                <div className={styles.look}>
+                  <FiSearch size={24} color="var(--gray-300)" />
+                </div>
+              </form>
+            ) : (
+              <NextLink href="/search" passHref>
+                <a>
+                  <form className={styles.searchForm}>
+                    <input
+                      type="search"
+                      id="search"
+                      data-target="search"
+                      placeholder="Search artist for book"
+                      autoFocus
+                    />
+                    <div className={styles.look}>
+                      <FiSearch size={24} color="var(--gray-300)" />
+                    </div>
+                  </form>
+                </a>
+              </NextLink>
+            )}
+          </div>
+
+          <ul>
+            <li>
+              <Products />
+            </li>
+            <li>
+              <button
+                className={`${styles.button} ${styles.signup}`}
+                type="button"
+              >
+                <span>Sign up</span>
+              </button>
+            </li>
+            <li>
+              <MoreOptions />
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      {disableSearchRedirect && (
+        <div className={styles.filter}>
+          <section>
+            <span>Principal localização</span>
+            <select name="location" id="location">
+              <option value="">Mundo</option>
+              <option value="1">São Paulo</option>
+              <option value="2">Rio de Janeiro</option>
+              <option value="3">Belo Horizonte</option>
+              <option value="4">Fortaleza</option>
+              <option value="5">Recife</option>
+            </select>
+          </section>
+        </div>
+      )}
+    </div>
   );
 }
--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
+
 import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
 
 import styles from "../styles/pages/Home.module.scss";
 
@@ -18,7 +20,54 @@ export default function Search() {
       <div id={styles.container}>
         <Header disableSearchRedirect />
 
-        <main></main>
+        <main>
+          <section className={styles.artists}>
+            <div className={styles.artistGroup}>
+              <h2>Artists</h2>
+
+              <div className={styles.cards}>
+                <div className={styles.card}>
+                  <img src="https://github.com/eliasallex.png" alt="artist" />
+                  <div className="card-content">
+                    <h3>Elias alexandre</h3>
+                  </div>
+                </div>
+                <div className={styles.card}>
+                  <img src="https://github.com/diego3g.png" alt="artist" />
+                  <div className="card-content">
+                    <h3>Elias alexandre</h3>
+                  </div>
+                </div>
+                <div className={styles.card}>
+                  <img src="https://github.com/maykbrito.png" alt="artist" />
+                  <div className="card-content">
+                    <h3>Elias alexandre</h3>
+                  </div>
+                </div>
+                <div className={styles.card}>
+                  <img src="https://github.com/mebjas.png" alt="artist" />
+                  <div className="card-content">
+                    <h3>Elias alexandre</h3>
+                  </div>
+                </div>
+                <div className={styles.card}>
+                  <img src="https://github.com/torvalds.png" alt="artist" />
+                  <div className="card-content">
+                    <h3>Elias alexandre</h3>
+                  </div>
+                </div>
+                <div className={styles.card}>
+                  <img src="https://github.com/Pipas.png" alt="artist" />
+                  <div className="card-content">
+                    <h3>Elias alexandre</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </main>
+
+        <Footer />
       </div>
     </>
   );
